0fd0620506e16b4effc79874f8d15f0d657fa13d
PBPL41 <jadyn.s.malone.25@dartmouth.edu>
2025-05-04 13:41:43 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/build-for-america/',
   server: {
     host: "::",
     port: 8080,
